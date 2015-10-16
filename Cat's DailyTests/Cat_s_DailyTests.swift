52986d738a0d81f2df8ff0f9c1c9bf2065ac5dd9
VoldLiu <liu_heyu@126.com>
2015-10-16 18:19:14 +0800

    first commit

---
+++ Cat's DailyTests/Cat_s_DailyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cat_s_DailyTests.swift
+//  Cat's DailyTests
+//
+//  Created by allen on 16/10/2015.
+//  Copyright © 2015 allen. All rights reserved.
+//
+
+import XCTest
+@testable import Cat_s_Daily
+
+class Cat_s_DailyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
